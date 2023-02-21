771d3c12e363960dc220763333675ca3117f5d63
BPNTT <haataya@163.com>
2023-02-22 01:52:27 +0600

    array length

---
+++ 001/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int  main ()
+{
+Console.WriteLine("Введите текст:");
+string[] textMass;
+string text = Console.ReadLine();
+textMass = text.Split(' ');
+int len = textMass.Length;
+Console.WriteLine("Количество слов:");
+Console.WriteLine($"{len}");
+return len;
+}
